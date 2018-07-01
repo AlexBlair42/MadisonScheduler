5e903f4bcb1f49c5ef9c619ca1a97930d4dc4340
Alex Blair <ablair18@my.whitworth.edu>
2018-06-30 23:36:56 -0700

    Add Art

---
+++ Application/SortingAlgorithm/MadisonScheduler/src/ArtTeacher.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MadisonScheduler
+{
+    class ArtTeacher : Teacher
+    {
+        public void print_Teacher_info()
+        {
+            Console.Out.WriteLine("This is an Art Specialist!");
+        }
+
+    }
+}
--- Docs/README.md
+++ Docs/README.md
@@ -4,3 +4,4 @@ Scheduling Application for Madison Elementary
 
 Created by Alex Blair, Eben Schumann, and Chris Blair-Garcia
 
+Ported over from C++
